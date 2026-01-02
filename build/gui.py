063b3aae4136b3fc350c1ac57a96bf2d3b554036
Arjun Singh Rahal <arjun.rahal@amla.io>
2026-01-02 15:12:53 +0530

    Fixed folder structure and re-uploaded

--- build/gui.py
+++ build/gui.py
@@ -9,7 +9,7 @@ import pydirectinput
 
 # from tkinter import *
 # Explicit imports to satisfy Flake8
-#first push c
+#first push cvvv
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 
 
